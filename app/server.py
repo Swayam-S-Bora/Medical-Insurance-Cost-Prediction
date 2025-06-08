84e4dc1347aab7f0725fc4e20f641f0b6f7596ac
Swayam-S-Bora <swayam.bora.309@gmail.com>
2025-06-08 13:35:28 +0530

    .

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 import joblib
@@ -15,6 +16,14 @@ class InsuranceInput(BaseModel):
 
 # FastAPI app
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
