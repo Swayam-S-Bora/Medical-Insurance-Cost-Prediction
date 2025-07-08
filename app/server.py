6b30a6a4542c0a4d83553865e3251de0f6425787
Swayam Singh Bora <swayam.bora.309@gmail.com>
2025-07-08 15:04:34 +0530

    server

--- app/server.py
+++ app/server.py
@@ -3,14 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import numpy as np
 import pandas as pd
-import shap
+# import shap
 import joblib
 
 # Load the model
 model = joblib.load("app/insurancemodel.pkl")
 
-# Initialize SHAP explainer
-explainer = shap.Explainer(model)
+# # Initialize SHAP explainer
+# explainer = shap.Explainer(model)
 
 # Input schema using Pydantic
 class InsuranceInput(BaseModel):
